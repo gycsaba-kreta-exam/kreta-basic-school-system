5df89163d8c03ef5ffff7b78b2bad1c2da3b3a2c
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-10-06 14:14:56 +0200

    User name binding

---
+++ KreataBasicSchholSystem.Desktop/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Security;
+
+namespace KreataBasicSchholSystem.Desktop.ViewModels
+{
+    partial class LoginViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _username;
+        [ObservableProperty]
+        private SecureString _password;
+
+        [ObservableProperty]
+        private string _errorMessage;
+        [ObservableProperty]
+        private bool _isViewVisible = true;
+    }
+}
