821b24869bbbf35a35affec3f11b0c66a28a45a0
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-10-10 10:34:24 +0200

    Az ablak fel, jobra, balra is húzható

--- KreataBasicSchoolSystem.Desktop/Views/MainView.xaml.cs
+++ KreataBasicSchoolSystem.Desktop/Views/MainView.xaml.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Interop;
+
 
 namespace KreataBasicSchoolSystem.Desktop.Views
 {
@@ -25,9 +23,13 @@ namespace KreataBasicSchoolSystem.Desktop.Views
             MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
         }
 
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
         private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            DragMove();
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
         }
     }
 }
