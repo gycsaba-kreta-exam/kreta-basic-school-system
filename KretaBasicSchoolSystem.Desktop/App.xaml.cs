59920f962f15353e262ab85f37ae48a7219c51d9
gycsaba <gyuris.csaba@vasvari.org>
2023-11-07 17:33:39 +0100

    Dotnet 7

--- KretaBasicSchoolSystem.Desktop/App.xaml.cs
+++ KretaBasicSchoolSystem.Desktop/App.xaml.cs
@@ -9,19 +9,32 @@ namespace KretaBasicSchoolSystem.Desktop
     /// </summary>
     public partial class App : Application
     {
+        private bool _loginPage = false;
+        //private readonly IHost host;
+
+
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-           /* var loginView = new LoginView();
-            loginView.Show();
-            loginView.IsVisibleChanged += (s, ev) =>
+            if (_loginPage)
             {
-                if (loginView.IsVisible == false && loginView.IsLoaded)
-                {*/
-                    var mainView = new MainView();
-                    mainView.Show();
-                   // loginView.Close();
-               // }
-           // };
+                var loginView = new LoginView();
+                loginView.Show();
+                loginView.IsVisibleChanged += (s, ev) =>
+                {
+                    if (loginView.IsVisible == false && loginView.IsLoaded)
+                    {
+                        var mainView = new MainView();
+                        mainView.Show();
+                        loginView.Close();
+                    }
+                };
+            }
+            else
+            {
+                var mainView = new MainView();
+                mainView.Show();
+            }            
         }
     }
 }
--- KretaBasicSchoolSystem.Desktop/ViewModels/MainViewModel.cs
+++ KretaBasicSchoolSystem.Desktop/ViewModels/MainViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using FontAwesome.Sharp;
+using KretaBasicSchoolSystem.Desktop.ViewModels.Base;
+using KretaBasicSchoolSystem.Desktop.ViewModels.ControlPanel;
 
 namespace KretaBasicSchoolSystem.Desktop.ViewModels
 {
-    public partial class MainViewModel : ObservableObject
+    public partial class MainViewModel : BaseViewModel
     {
         [ObservableProperty]
         private string _caption = string.Empty;
@@ -13,7 +15,7 @@ namespace KretaBasicSchoolSystem.Desktop.ViewModels
         private IconChar _icon = new IconChar();
 
         [ObservableProperty]
-        private ObservableObject _currentChildView;
+        private BaseViewModel _currentChildView;
 
         public MainViewModel()
         {
@@ -26,6 +28,7 @@ namespace KretaBasicSchoolSystem.Desktop.ViewModels
         {
             Caption = "Vezérlőpult";
             Icon=IconChar.Home;
+            CurrentChildView = new ControlPanelViewModel();
         }
     }
 }
